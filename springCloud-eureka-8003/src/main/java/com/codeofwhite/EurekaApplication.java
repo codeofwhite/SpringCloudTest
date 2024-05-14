f043dd6e6b12e2a7a7737f687918f39f2a018a70
zhj20 <99gelanjingling@gmail.com>
2024-05-14 12:25:46 +0800

    加了负载均衡，碰到client和ribbon的版本问题

---
+++ springCloud-eureka-8003/src/main/java/com/codeofwhite/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.codeofwhite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author codeofwhite
+ * @date 2024/5/14
+ * @Description
+ */
+@SpringBootApplication
+// 表示这个服务可以接受别人注册
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class , args);
+    }
+}
